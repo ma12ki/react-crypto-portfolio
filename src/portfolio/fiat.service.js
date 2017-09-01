e73ef2885c9db126877c77ff940c8452e551867f
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-09-01 18:57:54 +0200

    added localStorage methods for fiat

--- src/portfolio/fiat.service.js
+++ src/portfolio/fiat.service.js
@@ -2,11 +2,21 @@ import httpFactory from '../utils/httpFactory';
 
 import { baseCurrency } from '../config';
 
+const storageKeys = {
+    targetCurrency: 'TARGET_CURRENCY',
+};
+
 const http = httpFactory('https://api.fixer.io/');
 
 const getRate$ = (targetCurrency) => http.get$(`latest?base=${baseCurrency}&symbols=${targetCurrency}`)
     .map((res) => ({ currency: targetCurrency, rate: res.rates[targetCurrency] }));
 
+const storeTargetCurrency = (targetCurrency) => localStorage.setItem(storageKeys.targetCurrency, targetCurrency);
+const retrieveTargetCurrency = () => localStorage.getItem(storageKeys.targetCurrency) || baseCurrency;
+
 export {
     getRate$,
+
+    storeTargetCurrency,
+    retrieveTargetCurrency,
 };
