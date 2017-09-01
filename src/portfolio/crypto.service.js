97ee1d3c17bbecac9ea187fe70de1c4bceddb754
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-09-01 18:35:08 +0200

    added localStorage methods

--- src/portfolio/crypto.service.js
+++ src/portfolio/crypto.service.js
@@ -1,16 +1,33 @@
 import httpFactory from '../utils/httpFactory';
 
-const tickerStorageKey = 'TICKER';
+const storageKeys = {
+    ticker: 'TICKER',
+    selectedCryptos: 'SELECTED_CRYPTOS',
+    cryptoAmounts: 'CRYPTO_AMOUNTS',
+};
 
 const http = httpFactory('https://api.coinmarketcap.com/v1/');
 
 const getTicker$ = () => http.get$('ticker');
 
-const storeTicker = (tickerData) => localStorage.setItem(tickerStorageKey, tickerData);
-const retrieveTicker = () => localStorage.getItem(tickerStorageKey);
+const storeTicker = (tickerData) => localStorage.setItem(storageKeys.ticker, tickerData);
+const retrieveTicker = () => localStorage.getItem(storageKeys.ticker);
+
+const storeSelectedCryptos = (selectedCryptos) => localStorage.setItem(storageKeys.selectedCryptos, selectedCryptos);
+const retrieveSelectedCryptos = () => localStorage.getItem(storageKeys.selectedCryptos) || [];
+
+const storeCryptoAmounts = (cryptoAmounts) => localStorage.setItem(storageKeys.cryptoAmounts, cryptoAmounts);
+const retrieveCryptoAmounts = () => localStorage.getItem(storageKeys.cryptoAmounts) || {};
 
 export {
     getTicker$,
+
     storeTicker,
     retrieveTicker,
+
+    storeSelectedCryptos,
+    retrieveSelectedCryptos,
+
+    storeCryptoAmounts,
+    retrieveCryptoAmounts,
 };
